2ccd78fb74c5b8209c81b485a96c853db2286d42
Jacob Allen <jallen2033@gmail.com>
2021-01-15 05:51:03 +0000

    added fetchCoordsbyIP function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // index.js
-const { fetchMyIP } = require('./iss.js');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss.js');
 
+// return values error and ip in this callback function are already being handled in the backburner/event queue
 fetchMyIP((error, ip) => {
-  
+
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned IP:' , ip);
+  console.log(typeof ip);
+
+  // call and use fetchCoordsByIP function and feed it the IP address we got back from our server
+  fetchCoordsByIP(ip, ((error, latLong) => {
+    console.log(latLong);
+  }));
 });
--- iss.js
+++ iss.js
@@ -35,9 +35,41 @@ const fetchMyIP = function(callback) {
     // force the formatted ipAddress to be sent to the callback as the first incoming paramater
     const ipAddress = JSON.parse(body).ip;
     callback(null, ipAddress);
-
   });
 };
 
+
+// function that makes a single API request to retrieve the user's geo location based on their IP address.
+// in the function, make the request to the API, and have it pass back the relevant (lat/lng) data as an object via callback.
+const fetchCoordsByIP = function(ip, callback) {
+
+  console.log("ip: " + ip);
+  // https://freegeoip.app/json/216.180.65.66?callback=test
+  const geoLocationUrl = `https://freegeoip.app/json/${ip}`;
+
+  request(geoLocationUrl, (error, response, body) => {
+
+    // inside the request callback ...
+    // error can be set if there is an invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // if a non-200 status code is sent back from the server, assume there is a server error
+    // force the error to be sent to the callback as the first incoming paramater
+    if (response.statusCode !== 200) {
+      const msg = `${response.statusCode} wasencountered when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // parse the .latitute and .long in the JSOn object returned from the.replace(/(?:\[rn])+/g, '')
+    const parsedBody = JSON.parse(body);
+    const latLong = {lat: parsedBody.latitude, long: parsedBody.longitude}
+    callback(null, latLong);
+  });
+}
+
 // export the function fetchMyIP to be called and used at the index
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP: fetchMyIP, fetchCoordsByIP: fetchCoordsByIP };
