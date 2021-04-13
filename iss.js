0153532f70e91174f51cc278c7b463940aeae100
Jacob Allen <47609209+jallen2034@users.noreply.github.com>
2021-04-13 14:33:43 -0700

    Update iss.js
    Cleaned up comment style to make more presentable

--- iss.js
+++ iss.js
@@ -1,60 +1,60 @@
 const request = require('request');
 
-// makes a single API request to retrieve the user's IP address.Input:
-// a callback (to pass back an error or the IP string)
-// returns (via Callback):
-// an error, if any (nullable)
-// the IP address as a string (null if error). Example: "162.245.144.188"
-// use request to fetch IP address from JSON API
+/* makes a single API request to retrieve the user's IP address.Input:
+ * a callback (to pass back an error or the IP string)
+ * returns (via Callback):
+ * an error, if any (nullable)
+ * the IP address as a string (null if error). Example: "162.245.144.188"
+ * use request to fetch IP address from JSON API */
 const fetchMyIP = function(callback) {
 
   const ipifyUrl = 'https://api.ipify.org?format=json';
   request(ipifyUrl, (error, response, body) => {
 
-    // inside the request callback ...
-    // error can be set if there is an invalid domain, user is offline, etc.
+    /* inside the request callback ...
+    /* error can be set if there is an invalid domain, user is offline, etc. */
     if (error) {
       callback(error, null);
       return;
     }
 
-    // if a non-200 status code is sent back from the server, assume there is a server error
-    // force the error to be sent to the callback as the first incoming paramater
+    /* if a non-200 status code is sent back from the server, assume there is a server error
+     * force the error to be sent to the callback as the first incoming paramater */
     if (response.statusCode !== 200) {
       const msg = `${response.statusCode} wasencountered when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
 
-    // if we get here, all's well and we got the data
-    // call our callback function from line 4 of our index.js and pass it out any errors or the body from our http request
-    // parse the IP as JSON to be interpereted later
-    // force the formatted ipAddress to be sent to the callback as the first incoming paramater
+    /* if we get here, all's well and we got the data
+     * call our callback function from line 4 of our index.js and pass it out any errors or the body from our http request
+     * parse the IP as JSON to be interpereted later
+     * force the formatted ipAddress to be sent to the callback as the first incoming paramater */
     const ipAddress = JSON.parse(body).ip;
     callback(null, ipAddress);
   });
 };
 
 
-// function that makes a single API request to retrieve the user's geo location based on their IP address.
-// in the function, make the request to the API, and have it pass back the relevant (lat/lng) data as an object via callback.
-// https://freegeoip.app/json/216.180.65.66?callback=test
+/* function that makes a single API request to retrieve the user's geo location based on their IP address.
+ * in the function, make the request to the API, and have it pass back the relevant (lat/lng) data as an object via callback.
+ * https://freegeoip.app/json/216.180.65.66?callback=test */
 const fetchCoordsByIP = function(ip, callback) {
 
   const geoLocationUrl = `https://freegeoip.app/json/${ip}`;
 
   request(geoLocationUrl, (error, response, body) => {
 
-    // inside the request callback ...
-    // error can be set if there is an invalid domain, user is offline, etc.
-    // remember these callbacks go back into the nextISSTimesForMyLocation() function we passed here as an incoming param!
+    /* inside the request callback ...
+     * error can be set if there is an invalid domain, user is offline, etc.
+     * remember these callbacks go back into the nextISSTimesForMyLocation() function we passed here as an incoming param! */
     if (error) {
       callback(error, null);
       return;
     }
 
-    // if a non-200 status code is sent back from the server, assume there is a server error
-    // force the error to be sent to the callback as the first incoming paramater
+    /* if a non-200 status code is sent back from the server, assume there is a server error
+     * force the error to be sent to the callback as the first incoming paramater */
     if (response.statusCode !== 200) {
       const msg = `${response.statusCode} wasencountered when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
@@ -84,15 +84,15 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   
   request(flyoverUrl, (error, response, body) => {
 
-    // inside the request callback ...
-    // error can be set if there is an invalid domain, user is offline, etc.
+    /* inside the request callback ...
+     * error can be set if there is an invalid domain, user is offline, etc. */
     if (error) {
       callback(error, null);
       return;
     }
 
-    // if a non-200 status code is sent back from the server, assume there is a server error
-    // force the error to be sent to the callback as the first incoming paramater
+    /* if a non-200 status code is sent back from the server, assume there is a server error
+     * force the error to be sent to the callback as the first incoming paramater */
     if (response.statusCode !== 200) {
       const msg = `${response.statusCode} wasencountered when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
@@ -106,12 +106,12 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 // master function to deal deal with the "waterfall" of API calls inside of fetchMyIP(), fetchCoordsByIP(), fetchISSFlyOverTimes() nested inside
 const nextISSTimesForMyLocation = function(callback) {
 
-  // call fetchMyIP() passin in the below "waterfall" callback function as its one and only paramater
-  // remember "error" and "ip" are the arguments returned from the callback inside of fetchMyIP as the two incoming paramaters of the 
-  // anon function here on line 112
+ /* call fetchMyIP() passin in the below "waterfall" callback function as its one and only paramater
+  * remember "error" and "ip" are the arguments returned from the callback inside of fetchMyIP as the two incoming paramaters of the 
+  * anon function here on line 112 */
   fetchMyIP((error, ip) => {
 
-    // remember these callbacks go out to the callback function that we sent as an incoming paramater on line 113, back indo our index.js!
+    // remember these callbacks go out to the callback function that we sent as an incoming paramater on line 113, back into our index.js!
     if (error) {
       callback(error, null)
       return;
@@ -135,8 +135,8 @@ const nextISSTimesForMyLocation = function(callback) {
           return;
         }
         
-        // bottom layer of callbacks! if all above error checks passed, return the final flyoverTimes JSON to the callback function passed in to nextISSTimesForMyLocation
-        // to be printed later
+        /* bottom layer of callbacks! if all above error checks passed, return the final flyoverTimes JSON to the callback function passed in to nextISSTimesForMyLocation
+         * to be printed later */
         callback(null, flyoverTimes);
       });
     });  
