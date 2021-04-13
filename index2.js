5e97c670f11fe8e02a5194b3e9110136a637d305
Jacob Allen <47609209+jallen2034@users.noreply.github.com>
2021-04-13 14:30:42 -0700

    Update index2.js
    Cleaned up comments and code to make more presentable

--- index2.js
+++ index2.js
@@ -1,15 +1,14 @@
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised.js');
 
-// call the fetchMyIP() function here, calling this function returns a promise, this returned promise in turn resolves with the
-// body of whatever the http request inside retrieved from the server, remember the resoloution is abstracted away from us by the request libary!
+/* call fetchMyIP() function here, calling this function returns a promise, this returned promise in turn resolves with the
+ * body of whatever the http request inside retrieved from the server, remember the resoloution is abstracted away from us by the request libary! */
 fetchMyIP()
-  // remember the .then methods will only run if no errrors are encountered. The body which was previously resolved by the returned promise above now becomes IP address
-  // ipAdd in here is the body of whatever the incoming promise resolves with the above fetchMyIP() promise
+  /* the .then methods will only run if no errrors are encountered. The body which was previously resolved by the returned promise above now becomes IP address
+  /* ipAdd in here is the body of whatever the incoming promise resolves with the above fetchMyIP() promise */
   .then(ipAdd => {
     const ip = JSON.parse(ipAdd).ip;
     return fetchCoordsByIP(ip);
   }).then(geoData => {
-    console.log("Data??: " , geoData);
     const parsedBody = JSON.parse(geoData);
     const latLong = {lat: parsedBody.latitude, long: parsedBody.longitude};
     return fetchISSFlyOverTimes(latLong);
@@ -17,9 +16,8 @@ fetchMyIP()
     const flyoverTimesObj = JSON.parse(flyoverTimes);
     printPassTimes(flyoverTimesObj);
   })
-  // the erorr handling in this final .catch was already resolved in the returned promise from fetchMyIP(). If that resolved an error
-  // it is dumped here
-  // our catch block/condition at the end of our chain which will have a line drawn to it if any promises in the chain are rejected with errors
+  /* the erorr handling in this final .catch was already resolved in the returned promise from fetchMyIP(). If that resolved an error, it is dumped here
+   * our catch block/condition at the end of our chain which will have a line drawn to it if any promises in the chain are rejected with errors */
   .catch(error => {
     console.log(error);
   });
@@ -32,7 +30,6 @@ const printPassTimes = function(flyoverTimes) {
     const formattedDateTime = new Date(0);
     formattedDateTime.setUTCSeconds(objectInResponseArr.risetime);
     const formattedDuration = objectInResponseArr.duration;
-  
     console.log(`Next pass at ${formattedDateTime} for ${formattedDuration} seconds!`);
   }
 };
